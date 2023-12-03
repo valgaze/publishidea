3fd93bdc21c54afd5f0785ad825cd0ff6f59dfe7
Victor Algaze <valgaze@gmail.com>
2023-12-03 07:36:01 -0800

    import test

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,8 +4,8 @@ import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 import "./style.css";
 import { install } from "element-plus";
-import "element-plus/dist/index.css";
-import "element-plus/theme-chalk/dark/css-vars.css";
+// import "element-plus/dist/index.css";
+// import "element-plus/theme-chalk/dark/css-vars.css";
 
 export default {
   extends: DefaultTheme,
