7c8c8c350d24693112106e4a5b2e784b5ec54bd3
Victor Algaze <valgaze@gmail.com>
2023-12-05 21:29:03 -0800

    add clientonly to token handler, see about direct load hydration isse

--- docs/.vitepress/components/token_handler.vue
+++ docs/.vitepress/components/token_handler.vue
@@ -1,59 +1,63 @@
 <template>
-  <div>
-    <el-input
-      v-model="store.state.token"
-      @input="omg(store.state.token)"
-      placeholder="ex. MjhM5ZDVs3VJnsmMzVjYz..."
-      show-password
-      :autofocus="autofocus"
-    >
-      <template #prepend>
-        <el-button
-          @click="store.validateToken(store.state.token)"
-          type="warning"
-          :icon="Refresh"
-          :disabled="store.state.tokenValid"
-          >Check token</el-button
-        >
-      </template>
-    </el-input>
-    <el-alert
-      v-if="
-        store.state.tokenValid === false &&
-        store.state.tokenValid !== undefined &&
-        store.state.token.length > 5
-      "
-      title="😥 Token is INVALID (generate a new token and try again)"
-      type="error"
-    >
-    </el-alert>
-    <el-alert
-      v-if="store.state.tokenValid === true && store.state.token"
-      title="🎉 Token is good!"
-      type="success"
-    ></el-alert>
-    <el-descriptions
-      v-if="store.state.tokenValid === true && store.state.token && showInfo"
-      direction="vertical"
-      :column="3"
-    >
-      <el-descriptions-item label="Username">{{
-        store.state.userData.username
-      }}</el-descriptions-item>
-      <el-descriptions-item label="Chat Email(s)">
-        <a
-          type="primary"
-          v-for="(email, idx) in store.state.userData.emails"
-          :key="email + '_' + idx"
-          :href="'webexteams://im?email=' + email"
-          >{{ email }}
-        </a>
-      </el-descriptions-item>
-      <el-descriptions-item label="Type" :span="2"
-        ><el-tag>{{ store.state.userData.type }}</el-tag></el-descriptions-item
+  <client-only>
+    <div>
+      <el-input
+        v-model="store.state.token"
+        @input="omg(store.state.token)"
+        placeholder="ex. MjhM5ZDVs3VJnsmMzVjYz..."
+        show-password
+        :autofocus="autofocus"
       >
-    </el-descriptions>
-  </div>
+        <template #prepend>
+          <el-button
+            @click="store.validateToken(store.state.token)"
+            type="warning"
+            :icon="Refresh"
+            :disabled="store.state.tokenValid"
+            >Check token</el-button
+          >
+        </template>
+      </el-input>
+      <el-alert
+        v-if="
+          store.state.tokenValid === false &&
+          store.state.tokenValid !== undefined &&
+          store.state.token.length > 5
+        "
+        title="😥 Token is INVALID (generate a new token and try again)"
+        type="error"
+      >
+      </el-alert>
+      <el-alert
+        v-if="store.state.tokenValid === true && store.state.token"
+        title="🎉 Token is good!"
+        type="success"
+      ></el-alert>
+      <el-descriptions
+        v-if="store.state.tokenValid === true && store.state.token && showInfo"
+        direction="vertical"
+        :column="3"
+      >
+        <el-descriptions-item label="Username">{{
+          store.state.userData.username
+        }}</el-descriptions-item>
+        <el-descriptions-item label="Chat Email(s)">
+          <a
+            type="primary"
+            v-for="(email, idx) in store.state.userData.emails"
+            :key="email + '_' + idx"
+            :href="'webexteams://im?email=' + email"
+            >{{ email }}
+          </a>
+        </el-descriptions-item>
+        <el-descriptions-item label="Type" :span="2"
+          ><el-tag>{{
+            store.state.userData.type
+          }}</el-tag></el-descriptions-item
+        >
+      </el-descriptions>
+    </div>
+  </client-only>
 </template>
 
 <script lang="ts" setup>
